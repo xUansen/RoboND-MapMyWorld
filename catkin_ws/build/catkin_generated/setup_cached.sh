f90631438ddabc1c1cfdc031008a8c4db0eef876
xHe <xuan.he@columbia.edu>
2018-09-23 06:58:05 +0000

    initial

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export PKG_CONFIG_PATH="/home/nvidia/workspace/RoboND-WhereAmI/catkin_ws/devel/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/aarch64-linux-gnu/pkgconfig"
+export PWD="/home/nvidia/workspace/RoboND-MapMyWorld/catkin_ws/build"
+export PYTHONPATH="/home/nvidia/workspace/RoboND-WhereAmI/catkin_ws/devel/lib/python2.7/dist-packages:/opt/ros/kinetic/lib/python2.7/dist-packages"
+export ROS_PACKAGE_PATH="/home/nvidia/workspace/RoboND-MapMyWorld/catkin_ws/src:/home/nvidia/workspace/RoboND-WhereAmI/catkin_ws/src:/opt/ros/kinetic/share"
